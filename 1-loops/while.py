2d5f16beb155a5953b7522bea017fc8939909f7f
codio-units <spackham@codio.com>
2015-10-20 15:15:23 +0000

    Added Comments

--- 1-loops/while.py
+++ 1-loops/while.py
@@ -1,6 +1,7 @@
 
-counter = 0
+counter = 0                 # This is a 'counter' variable.
 
-while counter <= 9:
+while counter <= 9:         # If the condition is true, it loops back
   print(counter)
-  counter = counter + 1
+  counter = counter + 1     # Here we add 1 to the counter
+                            # This is known as 'incrementing' the counter.
--- 2-loops/utility.py
+++ 2-loops/utility.py
@@ -1,8 +1,9 @@
 
-counter = 1
-total = 0
+counter = 1                  # This is a 'counter' variable.
+total = 0                    # This is out utility variable 'totla'
 
-while counter <= 10:
-  total = total + counter
-  counter = counter + 1  
-  output(total)
+while counter <= 10:         # If the condition is true, it enters the loop
+  total = total + counter    # We add the current value of 'counter' to 'total'
+  counter = counter + 1      # Here we add 1 to the counter.
+                             # This is known as 'incrementing' the counter.
+  output(total)              # And finally, we output 'total'
