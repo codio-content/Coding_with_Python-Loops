1eae68c0eb8216223697daec1903efa9016c1a1e
codio-units <spackham@codio.com>
2015-10-20 15:11:13 +0000

    Change rnage() parameters on for loop example

--- 3-loops/for1.py
+++ 3-loops/for1.py
@@ -1,9 +1,9 @@
 
-input0 = input0(10)
-total = 0
+input0 = input0(10)  # load our special variable
+total = 0            # intialize our total variable
 
-for ctr in range(0, 10):
-  total = total + ctr
-  output(total)
+for ctr in range(0, input0 +1): # we use input0 +1 because range(0,10) = [0..9]
+  total = total + ctr           # increase our total by the loop amount
+  output(total)                 # output the total as far
   
-output('Final total : ' + str(total))
+output('Final total : ' + str(total)) # output the final tota;l
