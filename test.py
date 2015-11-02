d821f2417d218d320bd5441db1e1455677dde5d5
codio-units <spackham@codio.com>
2015-11-02 18:39:13 +0000

    updated to new version with better command line and output support

--- test.py
+++ test.py
@@ -1,25 +1,17 @@
 
 import sys
+import re
+import logging
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 inputs = []
 outputs = []
 
-def input0(v):
-  global inputs
-  return inputs[0]
-
-def input1(v):
-  global inputs
-  return inputs[1]
-
-def input2(v):
-  global inputs
-  return inputs[2]
-
-def output(v):
-  global outputs
-  outputs.append(v)
-  # print(v)
 
 def printException():
     exc_type, exc_obj, tb = sys.exc_info()
@@ -33,7 +25,33 @@ def test(_file, _inputs, _outputs, _message = 'Not quite right please try again'
   inputs = _inputs
   
   try:
-    exec(open(_file).read())
+    # redirect stdout to a string (fake_stdout)                                      
+    # compile into byte code and then execute the byte code                          
+    # then process the printed output from the code we ran                           
+    code= compile(open(_file).read(), '<string>', 'exec')                      
+    orig_stdout= sys.stdout
+    fake_stdout= StringIO()
+    sys.stdout= fake_stdout
+
+    # replace sys.argv while we exec the code
+    orig_argv= sys.argv
+    sys.argv= ['python', _file] 
+    for i in range(0, len(inputs)):
+      sys.argv.append(inputs[i])
+    exec(code)
+
+    # put everything back like it was before
+    sys.stdout= orig_stdout
+    sys.argv= orig_argv
+    sys.stderr.flush()
+
+    # set up the outputs
+    outputs= fake_stdout.getvalue().split("\n")		
+    last_line= outputs.pop()
+    if len(last_line.strip()) > 0:
+      outputs.append(last_line)
+
+    #print(outputs)
 
     if len(outputs) != len(_outputs):
       print('Your program is not outputting the expected number of outputs')
@@ -43,7 +61,8 @@ def test(_file, _inputs, _outputs, _message = 'Not quite right please try again'
     # print(_outputs)
       
     for i in range(len(outputs)):
-      if outputs[i] != _outputs[i]:
+      if str(outputs[i]) != str(_outputs[i]):
+        #print(str(outputs[i]) + " != " + str(_outputs[i]))
         print(_message)
         exit(1)
     
